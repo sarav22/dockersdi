88e6fa459a54cef9265ed46f5d974655932c2e76
Sara Vecino García <UO264093@uniovi.es>
2020-02-13 20:39:57 +0100

    SDI-SpringBoot- 4 -Acceso a datos con entidades relacionadas

--- sdi1920-414-lab-spring/src/main/java/com/uniovi/entities/Mark.java
+++ sdi1920-414-lab-spring/src/main/java/com/uniovi/entities/Mark.java
@@ -10,12 +10,22 @@ public class Mark {
 	private Long id;
 	private String description;
 	private Double score;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
 
 	public Mark(Long id, String description, Double score) {
 		super();
 		this.id = id;
 		this.description = description;
 		this.score = score;
+	}
+	public Mark( String description, Double score, User user) {
+		super();
+		this.description = description;
+		this.score = score;
+		this.user = user;
 	}
 
 	public Mark() {
@@ -46,6 +56,12 @@ public class Mark {
 		this.score = score;
 	}
 
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
 	@Override
 	public String toString() {
 		return "Mark [id=" + id + ", description=" + description + ", score=" + score + "]";
